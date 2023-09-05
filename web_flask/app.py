addaf999f92c63c9727e4f52b4f5c24c669e2b27
Provoski <provo.iyen27@gmail.com>
2023-09-05 12:21:48 +0000

    implement web route

---
+++ web_flask/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from flask import Flask, render_template, make_response, abort
+app = Flask(__name__)
+
+@app.route('/home', strict_slashes=False)
+def home_index():
+    """ render home page """
+    return render_template('landing_page.html')
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(debug=True)
