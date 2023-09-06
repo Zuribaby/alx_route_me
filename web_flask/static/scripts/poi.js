6d80ce69adc9fe59c349c78fa5f20ada5c641fb2
Provoski <provo.iyen27@gmail.com>
2023-09-06 19:04:00 +0000

    poi improve

--- web_flask/static/scripts/poi.js
+++ web_flask/static/scripts/poi.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hotelsSearch = document.getElementById("hotels");
+
     const hospitalsSearch = document.getElementById("hospitals");
     hotelsSearch.addEventListener("click", function () {
       fetchHotels();
@@ -12,58 +13,64 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function fetchHotels() {
-    const coord = getcoordinate();
-    var overpassUrl = 'https://overpass-api.de/api/interpreter';
-    var query = '[out:json][timeout:25];(node["amenity"="hotel"](latitude1,longitude1,latitude2,longitude2););out body;>;out skel qt;';
-    query = query.replace(/latitude1/g, coord.minLatitude)
-                 .replace(/longitude1/g, coord.minLongitude)
-                 .replace(/latitude2/g, coord.maxLatitude)
-                 .replace(/longitude2/g, coord.maxLongitude);
-    fetch(overpassUrl, {
-      method: 'POST',
-      body: query
-    })          
-    .then(response => response.json())
-    .then(data => {
-        // Loop through features and create markers for hospitals
-        console.log(data);
-        data.elements.forEach(hotel => {
-            const lat = hotel.lat;
-            const lon = hotel.lon;
-            // Create a marker for the hospital
-            //showAmenity(lat, lon);
-            var hotelsMarker = L.marker([lat, lon]).addTo(map).bindPopup("hotel");
-        });
-    })
-    .catch(error => console.error(error));
+  const coord = getcoordinate();
+  fetch('https://www.overpass-api.de/api/interpreter?', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'utf-8' // Change content type
+    },
+    body: `[out:json];node(${coord.minLatitude},${coord.minLongitude},${coord.maxLatitude},${coord.maxLongitude})[tourism=hotel];out;`
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    data.elements.forEach(hotel => {
+      const lat = hotel.lat;
+      const lon = hotel.lon;
+      // Create a marker for the hospital
+      map.setView([lat, lon], 12);
+      var hotel = L.marker([lat, lon]).addTo(map);
+    });
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 }
-
+  
 function fetchHospitals() {
-    const coord = getcoordinate();
-    console.log(coord.minLatitude);
-    var overpassUrl = 'https://overpass-api.de/api/interpreter';
-    var query = '[out:json][timeout:25];(node["amenity"="hospital"](latitude1,longitude1,latitude2,longitude2););out body;>;out skel qt;';
-    query = query.replace(/latitude1/g, coord.minLatitude)
-                 .replace(/longitude1/g, coord.minLongitude)
-                 .replace(/latitude2/g, coord.maxLatitude)
-                 .replace(/longitude2/g, coord.maxLongitude);
-    fetch(overpassUrl, {
-      method: 'POST',
-      body: query
-    })          
-    .then(response => response.json())
-    .then(data => {
-        // Loop through features and create markers for hospitals
-        console.log(data);
-        data.elements.forEach(hospital => {
-            const lat = hospital.lat;
-            const lon = hospital.lon;
-            // Create a marker for the hospital
-            //showAmenity(lat, lon);
-            var hospitalMarker = L.marker([lat, lon]).addTo(map).bindPopup("hospital");
-        });
-    })
-    .catch(error => console.error(error));
+  const coord = getcoordinate();
+  fetch('https://www.overpass-api.de/api/interpreter?', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'utf-8' // Change content type
+    },
+    body: `[out:json];node(${coord.minLatitude},${coord.minLongitude},${coord.maxLatitude},${coord.maxLongitude})[amenity=hospitel];out;`
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then((data) => {
+    data.elements.forEach(hospital => {
+      const lat = hospital.lat;
+      const lon = hospital.lon;
+      // Create a marker for the hospital
+      map.setView([lat, lon], 12);
+      var hospital = L.marker([lat, lon]).addTo(map);
+    });
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 }
 
 function getcoordinate () {
@@ -71,15 +78,31 @@ function getcoordinate () {
     const lo  = document.getElementById("lon");
     const lat = la.textContent;
     const lon = lo.textContent;
-    console.log(lat);
-    console.log(lon);
     // Central point (latitude, longitude)
-    const center = [parseFloat(lat), parseFloat(lon)];
+    const center = [lat, lon];
     console.log(center);
+    var latitude = null;
+    var longitude = null;
 
+    // Loop through the array elements and extract the values
+    for (var i = 0; i < center.length; i++) {
+      var parts = center[i].split(':'); // Split the string by ':'
+      if (parts.length === 2) {
+        var key = parts[0].trim(); // Trim any leading/trailing spaces
+        var value = parts[1].trim(); // Trim any leading/trailing spaces
+
+        // Check if the key is "Latitude" or "Longitude" and assign the value accordingly
+        if (key === 'Latitude') {
+          latitude = parseFloat(value);
+        } else if (key === 'Longitude') {
+        longitude = parseFloat(value);
+        }
+      }
+    }
+    console.log(latitude);
     // Radius in meters
     // Radius in kilometers
-    const radiusKm = 5;
+    const radiusKm = 5000;
 
     // Calculate bounding box coordinates
     const earthRadiusKm = 6371; // Approximate Earth radius in kilometers
@@ -89,10 +112,10 @@ function getcoordinate () {
     const lonDiff = (radiusKm / earthRadiusKm) * (180 / Math.PI) / Math.cos(center[0] * Math.PI / 180);
 
     // Calculate the minimum and maximum latitude and longitude
-    const minLat = center[0] - latDiff;
-    const maxLat = center[0] + latDiff;
-    const minLon = center[1] - lonDiff;
-    const maxLon = center[1] + lonDiff;
+    const minLat = latitude - latDiff;
+    const maxLat = latitude + latDiff;
+    const minLon = longitude - lonDiff;
+    const maxLon = longitude + lonDiff;
     return {
         minLatitude: minLat,
         maxLatitude: maxLat,
